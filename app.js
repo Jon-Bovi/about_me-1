21e6cb5c63235995a87be56140d586ffc1f40de7
sueanyv <valenciasue@hotmail.com>
2016-09-21 10:00:38 -0700

    changes to javascript

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 "use strict";
-var live= prompt('Do I live in seattle? Yes or No').toUpperCase();
-if (live==='YES') {
+var live = prompt('Do I live in seattle? Yes or No').toUpperCase();
+if (live ==='YES'||live ==="Y") {
     alert('Correct!');
-} else if (live==='NO') {
+} else if (live ==='NO'||live ==='N') {
       alert('Sorry you got it wrong!');
     }
       else {
@@ -12,9 +12,9 @@ if (live==='YES') {
 console.log('the user \'s answer is ' + live);
 
 var gold = prompt('Is my favorite color Gold? Yes or No').toUpperCase();
-if (gold==='YES') {
+if (gold ==='YES'||gold ==='Y') {
     alert('Correct!');
-} else if (gold==='NO') {
+} else if (gold ==='NO'|| gold ==='N') {
       alert('Sorry you got it wrong!');
     }
       else {
@@ -22,10 +22,10 @@ if (gold==='YES') {
 };
 console.log('the user \'s answer is ' + gold);
 
-var sing=prompt('Can I sing? Yes or No').toUpperCase();
-if (sing==='NO') {
+var sing = prompt('Can I sing? Yes or No').toUpperCase();
+if (sing ==='NO'|| sing === 'N') {
     alert('Correct!');
-} else if (sing==='NO') {
+} else if (sing ==='Yes' || sing === 'Y') {
       alert('Sorry you got it wrong!');
     }
       else {
@@ -33,10 +33,10 @@ if (sing==='NO') {
 };
 console.log('the user \'s answer is ' + sing);
 
-var pizza=prompt('Do I dislike pizza? Yes or No').toUpperCase();
-if (pizza==='YES') {
+var pizza = prompt('Do I dislike pizza? Yes or No').toUpperCase();
+if (pizza ==='YES'|| pizza === 'Y') {
     alert('Correct!');
-} else if (pizza==='NO') {
+} else if (pizza ==='NO'||pizza ==='N') {
       alert('Sorry you got it wrong!');
     }
       else {
@@ -44,10 +44,10 @@ if (pizza==='YES') {
 };
 console.log('the user \'s answer is ' + pizza);
 
-var dancing=prompt('Do I like  to dance? Yes or No').toUpperCase();
-if (dancing==='Yes') {
+var dancing = prompt('Do I like  to dance? Yes or No').toUpperCase();
+if (dancing ==='Yes'|| dancing ==="Y") {
     alert('Correct!');
-} else if (dancing==='NO') {
+} else if (dancing ==='NO'|| dancing ==="N") {
       alert('Sorry you got it wrong!');
     }
       else {
