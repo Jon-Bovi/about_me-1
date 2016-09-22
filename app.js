14ba4af4f480f6f037c64aa3ba20f2cd0905a23b
sueanyv <valenciasue@hotmail.com>
2016-09-22 00:37:05 -0700

    changes to game

--- app.js
+++ app.js
@@ -1,105 +1,120 @@
-"use strict";
+'use strict';
 
-
+var names = prompt('Hello! please enter your name.');
+alert(' Welcome to my page ' + names + '! Lets play a game, There are 7 question about me. lets see how many you can get correct. GOOD LUCK ');
 var score = 0;
 var live = prompt('Do I live in seattle? Yes or No').toUpperCase();
-if (live ==='YES'||live ==="Y") {
-    alert('Correct!');
+if (live === 'YES' || live === 'Y') {
+  alert('Correct!');
   score += 1;
-} else if (live ==='NO'||live ==='N') {
-      alert('Sorry you got it wrong!');
-
-    }
-      else {
-           alert('Wrong Input!');
-
+} else if (live === 'NO' || live === 'N') {
+  alert('Sorry you got it wrong!');
+} else {
+  alert('Wrong Input!');
 };
-alert('you have '+score+ '/ 7' )
-console.log('the user \'s answer is ' + live+ ','+ score  );
+
+console.log('the user \'s answer is ' + live + ' , ' + score);
 
 var gold = prompt('Is my favorite color Gold? Yes or No').toUpperCase();
-if (gold ==='YES'||gold ==='Y') {
-    alert('Correct!');
-     score += 1;
+if (gold === 'YES' || gold === 'Y') {
+  alert('Correct!');
+  score += 1;
 
-} else if (gold ==='NO'|| gold ==='N') {
-      alert('Sorry you got it wrong!');
+} else if (gold === 'NO' || gold === 'N') {
+  alert('Sorry you got it wrong!');
 
-    }
-      else {
-           alert('Wrong Input!');}
+} else {
+  alert('Wrong Input!');
+}
 
-          alert('you have '+score+ '/ 7' )
-console.log('the user \'s answer is ' + gold+ ','+ score  );
 
-/*var sing = prompt('Can I sing? Yes or No').toUpperCase();
-if (sing ==='NO'|| sing === 'N') {
-    alert('Correct!');
-} else if (sing ==='Yes' || sing === 'Y') {
-      alert('Sorry you got it wrong!');
-    }
-      else {
-           alert('Wrong Input!');
+console.log('the user \'s answer is ' + gold + ',' + score);
+
+var sing = prompt('Can I sing? Yes or No').toUpperCase();
+if (sing === 'NO' || sing === 'N') {
+  alert('Correct!');
+  score += 1;
+} else if (sing === 'Yes' || sing === 'Y') {
+  alert('Sorry you got it wrong!');
+} else {
+  alert('Wrong Input!');
 };
 console.log('the user \'s answer is ' + sing);
 
 var pizza = prompt('Do I dislike pizza? Yes or No').toUpperCase();
-if (pizza ==='YES'|| pizza === 'Y') {
-    alert('Correct!');
-} else if (pizza ==='NO'||pizza ==='N') {
-      alert('Sorry you got it wrong!');
-    }
-      else {
-           alert('Wrong Input!');
+if (pizza === 'YES' || pizza === 'Y') {
+  alert('Correct!');
+  score += 1;
+} else if (pizza === 'NO' || pizza === 'N') {
+  alert('Sorry you got it wrong!');
+} else {
+  alert('Wrong Input!');
 };
 console.log('the user \'s answer is ' + pizza);
 
 var dancing = prompt('Do I like  to dance? Yes or No').toUpperCase();
-if (dancing ==='YES'|| dancing ==="Y") {
-    alert('Correct!');
-} else if (dancing ==='NO'|| dancing ==="N") {
-      alert('Sorry you got it wrong!');
-    }
-      else {
-           alert('Wrong Input!');
+if (dancing === 'YES' || dancing === 'Y') {
+  alert('Correct!');
+  score += 1;
+} else if (dancing === 'NO' || dancing === 'N') {
+  alert('Sorry you got it wrong!');
+} else {
+  alert('Wrong Input!');
 };
+
 console.log('the user \'s answer is ' + dancing);
 
 
 var counter = 0;
-while (counter < 4){
-  var input = parseInt(prompt('Can you guess how many mini hershey chocolates I ate today?'));
-  if (input === 4) {
-    alert ('Correct!');
+while (counter < 4) {
+  var number = parseInt(prompt('I am thinking of a random number between 1 & 6?.'));
+  var topnumber = Math.floor(Math.random() * 6) + 1;
+  if (topnumber == number) {
+    alert('Good Job!');
+    score += 1;
     break;
-  } else if (input < 4) {
-    alert(' You guessed too low!');
   } else {
-    alert('You guessed too high');
-  }
-counter++;
+    alert(' try again the number was ' + topnumber);
+    counter += 1;
+  };
 }
-console.log('the user \'s answer is ' + input);
 
-var counter = 0;
-while (counter < 6){
-  var places = ['Texas', 'Pennsylvania' , 'Maryland'];
-  var state = prompt('Can you guess a state that I have lived in besides Washington?');
+
+console.log('the user \'s answer is ' + number);
+
+var counter1 = 0;
+while (counter1 < 6) {
+  var places = ['TEXAS', 'PENNSYLVANIA', 'MARYLAND'];
+  var state = prompt('Can you guess a state that I have lived in besides Washington?').toUpperCase();
   var answerwasfound = false;
-  for (var i = 0; i < places.length; i++){
+  for (var i = 0; i < places.length; i++) {
     console.log(places.length);
-    places[i]
-    if (state === places[i]){
+    places[i];
+    if (state === places[i]) {
       answerwasfound = true;
-        break;
-    }}
+      score += 1;
+      break;
+    }
+  }
 
-  if(answerwasfound === true)  {
+  if (answerwasfound === true) {
     alert('You are correct, I have lived in Texas, Pennsylvania , Maryland');
     break;
   } else {
     alert('You are wrong');
-    counter++;
-  }}
 
-console.log('the user \'s answer is ' + input);*/
+  }
+}
+if (counter1 === 6) {
+  alert('Sorry! you are out of guesses for this question. The other states I have lived in are Texas, Pennsylvania , Maryland');
+};
+if (score === 7) {
+  alert('Congratulations!' + names + ' you got' + score + '/ 7 questions correct');
+
+} else {
+  alert('Sorry' + names + 'you got ' + score + '/7 questions correct');
+}
+
+
+
+console.log('the user \'s answer is ' + state);
