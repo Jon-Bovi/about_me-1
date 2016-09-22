c3635d9fd9fdac8c42f55c5a5abbd65ab12d8735
Ford Fowler <fordjfowler@gmail.com>
2016-09-22 13:50:05 -0700

    make functions for questions 1-6

--- app.js
+++ app.js
@@ -1,85 +1,121 @@
 'use strict';
 
-
-
-var names = prompt('Hello! please enter your name.');
-alert(' Welcome to my page ' + names + '! Lets play a game, There are 7 question about me. lets see how many you can get correct. GOOD LUCK ');
 var score = 0;
-var live = prompt('Do I live in seattle? Yes or No').toUpperCase();
-if (live === 'YES' || live === 'Y') {
-  alert('Correct!');
-  score += 1;
-} else if (live === 'NO' || live === 'N') {
-  alert('Sorry you got it wrong!');
-} else {
-  alert('Wrong Input!');
-};
+var questions = ['Do I live in Seattle? Yes or No.', 'Is my favorite color Gold? Yes or No', 'Can I sing? Yes or No', 'Do I dislike pizza? Yes or No', 'Do I like to dance? Yes or No.', 'I am thinking of a random number between 1 & 6?.'];
+var answers = [['YES', 'Y'], ['YES', 'Y'], ['NO', 'N'], ['YES', 'Y'], ['YES', 'Y']];
 
-console.log('the user \'s answer is ' + live + ' , ' + score);
-
-var gold = prompt('Is my favorite color Gold? Yes or No').toUpperCase();
-if (gold === 'YES' || gold === 'Y') {
-  alert('Correct!');
-  score += 1;
-
-} else if (gold === 'NO' || gold === 'N') {
-  alert('Sorry you got it wrong!');
-
-} else {
-  alert('Wrong Input!');
-}
-
-
-console.log('the user \'s answer is ' + gold + ',' + score);
-
-var sing = prompt('Can I sing? Yes or No').toUpperCase();
-if (sing === 'NO' || sing === 'N') {
-  alert('Correct!');
-  score += 1;
-} else if (sing === 'Yes' || sing === 'Y') {
-  alert('Sorry you got it wrong!');
-} else {
-  alert('Wrong Input!');
-};
-console.log('the user \'s answer is ' + sing);
-
-var pizza = prompt('Do I dislike pizza? Yes or No').toUpperCase();
-if (pizza === 'YES' || pizza === 'Y') {
-  alert('Correct!');
-  score += 1;
-} else if (pizza === 'NO' || pizza === 'N') {
-  alert('Sorry you got it wrong!');
-} else {
-  alert('Wrong Input!');
-};
-console.log('the user \'s answer is ' + pizza);
-
-var dancing = prompt('Do I like  to dance? Yes or No').toUpperCase();
-if (dancing === 'YES' || dancing === 'Y') {
-  alert('Correct!');
-  score += 1;
-} else if (dancing === 'NO' || dancing === 'N') {
-  alert('Sorry you got it wrong!');
-} else {
-  alert('Wrong Input!');
-};
-
-console.log('the user \'s answer is ' + dancing);
-
-
-var counter = 0;
-while (counter < 4) {
-  var number = parseInt(prompt('I am thinking of a random number between 1 & 6?.'));
-  var topnumber = Math.floor(Math.random() * 6) + 1;
-  if (topnumber == number) {
-    alert('Good Job!');
-    score += 1;
-    break;
+function questions1to5 (questionNumber) {
+  var answer = prompt(questions[questionNumber]).toUpperCase();
+  if (answer === answers[questionNumber][0] || answer === answers[questionNumber][1]) {
+    alert('Correct!');
+    score++;
   } else {
-    alert(' try again the number was ' + topnumber);
-    counter += 1;
-  };
+    alert('Sorry you got it wrong!');
+  }
+};
+
+for (var i = 0; i < 5; i++) {
+  questions1to5(i);
 }
+
+
+// var names = prompt('Hello! please enter your name.');
+// alert(' Welcome to my page ' + names + '! Lets play a game, There are 7 question about me. lets see how many you can get correct. GOOD LUCK ');
+//
+// var live = prompt('Do I live in seattle? Yes or No').toUpperCase();
+// if (live === 'YES' || live === 'Y') {
+//   alert('Correct!');
+//   score += 1;
+// } else if (live === 'NO' || live === 'N') {
+//   alert('Sorry you got it wrong!');
+// } else {
+//   alert('Wrong Input!');
+// };
+//
+// console.log('the user \'s answer is ' + live + ' , ' + score);
+//
+// var gold = prompt('Is my favorite color Gold? Yes or No').toUpperCase();
+// if (gold === 'YES' || gold === 'Y') {
+//   alert('Correct!');
+//   score += 1;
+//
+// } else if (gold === 'NO' || gold === 'N') {
+//   alert('Sorry you got it wrong!');
+//
+// } else {
+//   alert('Wrong Input!');
+// }
+//
+//
+// console.log('the user \'s answer is ' + gold + ',' + score);
+//
+// var sing = prompt('Can I sing? Yes or No').toUpperCase();
+// if (sing === 'NO' || sing === 'N') {
+//   alert('Correct!');
+//   score += 1;
+// } else if (sing === 'Yes' || sing === 'Y') {
+//   alert('Sorry you got it wrong!');
+// } else {
+//   alert('Wrong Input!');
+// };
+// console.log('the user \'s answer is ' + sing);
+//
+// var pizza = prompt('Do I dislike pizza? Yes or No').toUpperCase();
+// if (pizza === 'YES' || pizza === 'Y') {
+//   alert('Correct!');
+//   score += 1;
+// } else if (pizza === 'NO' || pizza === 'N') {
+//   alert('Sorry you got it wrong!');
+// } else {
+//   alert('Wrong Input!');
+// };
+// console.log('the user \'s answer is ' + pizza);
+//
+// var dancing = prompt('Do I like  to dance? Yes or No').toUpperCase();
+// if (dancing === 'YES' || dancing === 'Y') {
+//   alert('Correct!');
+//   score += 1;
+// } else if (dancing === 'NO' || dancing === 'N') {
+//   alert('Sorry you got it wrong!');
+// } else {
+//   alert('Wrong Input!');
+// };
+//
+// console.log('the user \'s answer is ' + dancing);
+
+function question6 () {
+  var counter = 0;
+  var topnumber = Math.floor(Math.random() * 6) + 1;
+  console.log(topnumber);
+  while (counter < 4) {
+    var number = parseInt(prompt('I am thinking of a random number between 1 & 6?.'));
+    if (topnumber === number) {
+      alert('Good Job!');
+      score += 1;
+      break;
+    } else {
+      alert('try again.');
+      counter += 1;
+    };
+  }
+}
+
+question6();
+
+//Question 6
+// var counter = 0;
+// var topnumber = Math.floor(Math.random() * 6) + 1;
+// while (counter < 4) {
+//   var number = parseInt(prompt('I am thinking of a random number between 1 & 6?.'));
+//   if (topnumber === number) {
+//     alert('Good Job!');
+//     score += 1;
+//     break;
+//   } else {
+//     alert(' try again.');
+//     counter += 1;
+//   };
+// }
 
 
 console.log('the user \'s answer is ' + number);
